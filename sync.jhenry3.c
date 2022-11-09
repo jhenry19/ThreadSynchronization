001a0b8d36edd6f8cbd6b5c256ec4857186ce6f5
jhenry19 <jhny277@gmail.com>
2022-11-09 08:54:06 -0500

    finished adding comments and updated consumer stop logic so any negative number from producer triggers it

--- sync.jhenry3.c
+++ sync.jhenry3.c
@@ -1,13 +1,11 @@
-#include <printf.h>
 #include "sync.jhenry3.h"
-
 
 int insertItem(Buffer *buffer, int element) {
     int rtnval = 1;
-    pthread_mutex_lock(&buffer->lock);
-    if (buffer->counter < MAX && buffer->readyToRead == false) {
+    pthread_mutex_lock(&buffer->lock); // Lock acquired because the shared buffer is accessed
+    if (buffer->counter < MAX && buffer->readyToRead == false) { // check if over capacity or the last value hasn't been read
         buffer->buf[buffer->counter] = element;
-        buffer->readyToRead = true;
+        buffer->readyToRead = true; // changes so consumer knows a new element has been added
         buffer->counter += 1;
         rtnval = 0;
         printf("producer wrote the value %d\n", element);
@@ -18,39 +16,36 @@ int insertItem(Buffer *buffer, int element) {
 
 int removeItem(Buffer *buffer, int *element) {
     int rtnval = 1;
-    pthread_mutex_lock(&buffer->lock);
+    pthread_mutex_lock(&buffer->lock); // Lock is used because shared buffer is accessed
     if (buffer->counter > 0 && buffer->readyToRead == true) {
-        if (buffer->buf[buffer->counter - 1] == -1) {
+        if (buffer->buf[buffer->counter - 1] < 0) { // if producer is done, it puts a negative number in the buffer
             rtnval = 2;
             printf("consumer read the value %d\n", buffer->buf[buffer->counter - 1]);
         }
-        else {
+        else { // consuming a value the consumer has produced and adding it to sum
             buffer->sum += buffer->buf[buffer->counter - 1];
             buffer->readyToRead = false;
             rtnval = 0;
             printf("consumer read the value %d\n", buffer->buf[buffer->counter - 1]);
         }
-
     }
     pthread_mutex_unlock(&buffer->lock);
     return rtnval;
 }
 
 /**
- * Sends N values: the integers 0 through N-1. When the producer is done, it should send a negative
- * integer: that will tell the consumer to print a summary (the number of values sent, and the sum of all of the
- * values). For this program, “sending a value” means making it available for the consumer to read.
+ * Sends N values: the integers 0 through N-1. When the producer is done, it sends a negative integer if done producing
  */
 void *producer(void *param) {
     Buffer *buffer = (Buffer *) param;
     int sts;
-    for (int i = 0; i < N + 1; ++i) {
+    for (int i = 0; i <= N; ++i) {
         bool tryToInsert = true;
-        while(tryToInsert) {
-            if (i == N) sts = insertItem(buffer, -1);
+        while(tryToInsert) { // tries to insert until successful
+            if (i == N) sts = insertItem(buffer, -1); // inserts -1 on last run
             else sts = insertItem(buffer, i);
 
-            if (sts == 0) {
+            if (sts == 0) { // stops trying to insert if the insert is succesfull
                 tryToInsert = false;
             }
         }
@@ -67,18 +62,17 @@ void *consumer(void *param) {
         bool tryToRead = true;
         while(tryToRead) {
             sts = removeItem(buffer, &counter);
-            if (sts == 0) {
+            if (sts == 0) { // If a normal element is read (element >= 0)
                 ++counter;
                 tryToRead = false;
             }
-            else if (sts == 2) { // Given when -1 is read by consumer
-                pthread_mutex_lock(&buffer->lock);
+            else if (sts == 2) { // If a negative number is read
+                pthread_mutex_lock(&buffer->lock); // lock used because shared resource is accessed
                 printf("#samples = %d\n", counter);
                 printf("sum = %d\n", buffer->sum);
                 pthread_mutex_unlock(&buffer->lock);
-                tryToRead = false;
-                consuming = false;
-
+                tryToRead = false; // Quits this reads while loop
+                consuming = false; // Quits main while loop
             }
         }
     }
@@ -90,19 +84,18 @@ int main() {
     // Initialize data structures and threads
     Buffer buffer;
     pthread_t producerThread, consumerThread;
-
     buffer.counter = 0;
     buffer.sum = 0;
     buffer.readyToRead = false;
     pthread_mutex_init(&buffer.lock, NULL);
 
+    // Start threads
     pthread_create(&producerThread, NULL, producer, &buffer);
     pthread_create(&consumerThread, NULL, consumer, &buffer);
 
+    // Join threads
     pthread_join(producerThread, NULL);
     pthread_join(consumerThread, NULL);
-
-
 
     return 0;
 }
--- sync.jhenry3.h
+++ sync.jhenry3.h
@@ -2,15 +2,14 @@
 #define THREADSYNCHRONIZATION_SYNC_JHENRY3_H
 #include <pthread.h>
 #include <stdbool.h>
+#include <printf.h>
 #define N 10
 #define MAX 80
 
-
-typedef struct {
-    int cumulativeSum;
-    int count;
-} AvgStruct;
-
+/**
+ * Struct for shared resource between threads. A boolean is used to flag whose turn it is to produce or consume from
+ * the buffer
+ */
 typedef struct {
     int buf[MAX];
     int counter;
@@ -19,8 +18,26 @@ typedef struct {
     pthread_mutex_t lock;
 } Buffer;
 
+/**
+ * Inserts an element into the shared buffer
+ * @return 0 if successful, 1 if did not complete insertion
+ */
 int insertItem(Buffer *buffer, int element);
+
+/**
+ * Removes an element from the shared buffer
+ * @return 0 if successful, 1 if no new element was in buffer, 2 if value is -1 meaning producers is done
+ */
+int removeItem(Buffer *buffer, int *element);
+
+/**
+ * The main execution of the producer, which manages when to run insertItem
+ */
 void *producer(void *param);
+
+/**
+ * The main execution of the consumer, which interprets the results of removeItem
+ */
 void *consumer(void *param);
 
 
